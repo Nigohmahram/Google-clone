ef31a5316aeb733c2aa25a7cbb0591b1fc63e011
Usmonov Hojiakbar <usmonovmaxsudovich@gmail.com>
2023-11-17 22:21:59 +0900

    React-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import React from 'react';
 import './App.css';
 import Home from './pages/Home';
+import { Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import SearchPage from './pages/SearchPage';
 
 function App() {
 	return (
-		<div className='App'>
-			<Home />
-		</div>
+		<Router>
+			<div className='App'>
+				<Routes>
+					<Route path='/' element={<Home />}></Route>
+					<Route path='/search' element={<SearchPage />}></Route>
+				</Routes>
+			</div>
+		</Router>
 	);
 }
 
